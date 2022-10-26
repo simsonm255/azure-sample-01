52356e05997724ed1f3fe8815c24c03b21d75568
simson255 <sims91502@gmail.com>
2022-10-26 20:12:59 +0530

    sssaleld

--- src/main/java/com/example/azuresample01/AzureSample01Application.java
+++ src/main/java/com/example/azuresample01/AzureSample01Application.java
@@ -2,12 +2,19 @@ package com.example.azuresample01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AzureSample01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureSample01Application.class, args);
 	}
 
+	@GetMapping("/message")
+	public String getMessage(){
+		return "hi hello welcome azure cloud";
+	}
 }

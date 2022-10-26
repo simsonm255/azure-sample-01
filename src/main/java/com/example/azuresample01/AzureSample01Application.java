b240f4755588fd7c9a3eede680bb265e17c26ea3
simson255 <sims91502@gmail.com>
2022-10-26 20:09:40 +0530

    Initial commit

---
+++ src/main/java/com/example/azuresample01/AzureSample01Application.java
@@ -0,0 +1,13 @@
+package com.example.azuresample01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSample01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSample01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/azuresample01/AzureSample01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azuresample01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSample01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
